2647c555e407be59840096d44b7bc44ed60aa4d0
Carlos Arturo Vargas <carlosospino13@gmail.com>
2020-02-12 18:13:32 -0500

    Proyecto terminado

--- src/equiposinexistentes/EquiposInexistentes.java
+++ src/equiposinexistentes/EquiposInexistentes.java
@@ -1,6 +1,8 @@
 package equiposinexistentes;
 
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class EquiposInexistentes {
 
@@ -8,7 +10,28 @@ public class EquiposInexistentes {
         
         String path = "C:\\archivosJava\\Equipos que ya no existen.txt";
         
-        File file = new File(path);
+        File fileInput = new File(path);
+        File fileOutput = new File("resultado.txt");
+        try {
+            BufferedReader entrada = new BufferedReader(new FileReader(fileInput));
+            String linea = entrada.readLine();
+            
+            PrintWriter salida = new PrintWriter(new FileWriter(fileOutput));
+            while(linea != null) {
+                if(linea.contains("Unable to resolve host")) {
+                    salida.println(linea.substring(66).replace("\"!", ""));
+                }
+                linea = entrada.readLine();
+            }
+            entrada.close();
+            salida.close();
+            System.out.println("Archivo creado exitosamente.");
+        } catch (FileNotFoundException ex) {
+            System.err.println("Error: Archivo no encontrado");
+        } catch (IOException ex) {
+            System.err.println("Error al leer el archivo");
+        }
+        
     }
     
 }
