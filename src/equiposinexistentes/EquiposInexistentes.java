f0cf0dedddceda015346698c3c92cdda9d1caaca
Carlos Arturo Vargas <carlosospino13@gmail.com>
2020-02-12 17:43:07 -0500

    Commit inicial

---
+++ src/equiposinexistentes/EquiposInexistentes.java
@@ -0,0 +1,14 @@
+package equiposinexistentes;
+
+import java.io.*;
+
+public class EquiposInexistentes {
+
+    public static void main(String[] args) {
+        
+        String path = "C:\\archivosJava\\Equipos que ya no existen.txt";
+        
+        File file = new File(path);
+    }
+    
+}
